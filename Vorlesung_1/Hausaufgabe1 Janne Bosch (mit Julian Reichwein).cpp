4532c4681d9a810a612f58bbe5628ca469505c3e
jannikwiessler <56755425+jannikwiessler@users.noreply.github.com>
2020-05-12 21:43:23 +0200

    -1P because of missing abort criteria of max iter -> 8/10

--- Vorlesung_1/Hausaufgabe1 Janne Bosch (mit Julian Reichwein).cpp
+++ Vorlesung_1/Hausaufgabe1 Janne Bosch (mit Julian Reichwein).cpp
@@ -50,7 +50,8 @@ int main()
 /*
 JW-2020-05-12:
 --------------
-> nice implementation! (9/10) 
-> missing extra lib (*.c + *.h) incl. includegard includeguards: -1 Pkt
+> nice implementation! (8/10) 
+> missing extra lib (*.c + *.h) incl. includegard includeguards: -1 P
+> missing abort criteria of max iterations: -1P
 > please insert *.c files instead of .cpp next time :-) thanks !
 */
